45d7e2fd727b2e20baa69316535dc32c4c8aba5c
Matt Searle <mgsearle@proton.me>
2026-01-22 16:03:08 +0000

    Remove unused imports and update navigation links
    - Remove unused React hooks (useEffect, useState) from ValidationMessage component
    - Update NavGrid links from placeholder '#' to actual routes ('/patient-booking', '/doctor-login')

--- client/src/Components/HomeComponents/NavGrid/index.js
+++ client/src/Components/HomeComponents/NavGrid/index.js
@@ -8,12 +8,12 @@ const NavGrid = ()  => {
                     <h1 className='text-2xl lg:text-6xl text-white text-center'>Book an appointment</h1>
                 </div>
             </a>
-            <a href='#' className='col-start-1 col-span-full md:col-span-3 row-start-3 md:row-start-4 md:row-span-2 bg-diary bg-center bg-cover bg-no-repeat bg-blue-400 bg-blend-multiply rounded-md transform duration-500 ease-in-out hover:scale-105'>
+            <a href='/patient-booking' className='col-start-1 col-span-full md:col-span-3 row-start-3 md:row-start-4 md:row-span-2 bg-diary bg-center bg-cover bg-no-repeat bg-blue-400 bg-blend-multiply rounded-md transform duration-500 ease-in-out hover:scale-105'>
                 <div className='flex justify-center items-center h-full'>
                     <h1 className='text-2xl lg:text-6xl text-white text-center'>Change appointment</h1>
                 </div>
             </a>
-            <a href='#' className='col-start-1 md:col-start-4 col-span-full md:col-span-1 row-start-4 md:row-span-2 bg-doctor bg-center bg-cover bg-no-repeat bg-blue-400 bg-blend-multiply rounded-md transform duration-500 ease-in-out hover:scale-105'>
+            <a href='/doctor-login' className='col-start-1 md:col-start-4 col-span-full md:col-span-1 row-start-4 md:row-span-2 bg-doctor bg-center bg-cover bg-no-repeat bg-blue-400 bg-blend-multiply rounded-md transform duration-500 ease-in-out hover:scale-105'>
                 <div className='flex justify-center items-center h-full'>
                     <h1 className='text-xl lg:text-4xl text-white text-center'>Doctor's Portal</h1>
                 </div>
