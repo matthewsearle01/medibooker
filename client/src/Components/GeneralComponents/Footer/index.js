fb755214b6b6376c28f97c62bdb0eebca8f32d26
Matt Searle <mgsearle@proton.me>
2026-01-22 16:41:09 +0000

    Remove logos

--- client/src/Components/GeneralComponents/Footer/index.js
+++ client/src/Components/GeneralComponents/Footer/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Aardvarks from '../../../Assets/Images/Angry Aardvarks Logo.svg'
 
 const Footer = ()  => {
     return (
         <footer className='bg-blue-100 h-12 md:h-16 flex justify-center items-center'>
-            <img src={Aardvarks} alt='Angry Aardvarks logo' className='max-h-full w-100 py-1'/>
-            <p className='text-blue-900'>&copy; 2021</p>
+            <p className='text-blue-900'>MediBooker</p>
         </footer>
     );
 };
