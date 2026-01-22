a289e1b6dabf4bc30bbca1394fe5f09d8b8e623e
Matt Searle <mgsearle@proton.me>
2026-01-22 16:21:15 +0000

    Fix MongoDB connection - lazy init with timeouts

--- api/lib/mongo.js
+++ api/lib/mongo.js
@@ -1,18 +1,23 @@
 const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGODB_URI;
-if (!uri) {
-  console.warn('MONGODB_URI not set');
-}
-
 let clientPromise;
-if (!global._mongoClientPromise) {
-  const client = new MongoClient(uri, { maxPoolSize: 10 });
-  global._mongoClientPromise = client.connect();
-}
-clientPromise = global._mongoClientPromise;
 
 async function getDb(name = 'medibookerdb') {
+  const uri = process.env.MONGODB_URI;
+  
+  if (!uri) {
+    throw new Error('MONGODB_URI environment variable is not set');
+  }
+
+  if (!clientPromise) {
+    const client = new MongoClient(uri, { 
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      connectTimeoutMS: 10000
+    });
+    clientPromise = client.connect();
+  }
+  
   const client = await clientPromise;
   return client.db(name);
 }
