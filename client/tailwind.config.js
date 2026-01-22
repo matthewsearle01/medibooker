cdcbb3d7d79b96d250a703ff922cdf56e0868644
Matt Searle <mgsearle@proton.me>
2026-01-22 16:19:14 +0000

    Add Tailwind safelist for background image classes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  safelist: ['bg-booking', 'bg-diary', 'bg-doctor'],
   theme: {
     extend: {
       colors: {
